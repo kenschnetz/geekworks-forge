31c2e0e763cce4e45e6e36296cd059788ef54437
Ken <ken@syntaxflow.com>
2021-12-15 00:34:36 -0800

    Added application terms view

--- resources/js/Pages/Terms.js
+++ resources/js/Pages/Terms.js
@@ -2,8 +2,20 @@ import React from 'react';
 import Authenticated from '@/Layouts/Authenticated';
 import { Head } from '@inertiajs/inertia-react';
 
-export default function Dashboard(props) {
+const iframeWrapper = {
+    width: '100%',
+    maxWidth: '820px',
+    margin: '0 auto',
+}
+
+const iframe = {
+    height: '100vh',
+}
+
+export default function Terms(props) {
     return (
-        <h1>Terms</h1>
+        <div style={iframeWrapper}>
+            <iframe className="text-center w-full" style={iframe} src="https://docs.google.com/document/d/e/2PACX-1vTqvKDlX2tYMrkgWh5l0Lmkf7Gt_zvND4nZLlTFgR9opFF9QC1l67mE3wMXxYKuw4EWOmFou4Q_x6Db/pub?embedded=true"></iframe>
+        </div>
     );
 }
