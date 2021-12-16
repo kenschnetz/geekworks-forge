44f93cb76585a2921d664d2b0671821e280bb014
Ken <ken@syntaxflow.com>
2021-12-15 22:58:02 -0800

    Updated user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,63 +45,7 @@
             'email_verified_at' => 'datetime',
         ];
 
-        public function Muted() {
-            return $this->hasOne(UserMute::class)->where('expiration', '>', Carbon::now())->exists();
-        }
-
-        public function Banned() {
-            return $this->hasOne(UserBan::class)->exists();
-        }
-
-        public function Role() {
-            return $this->belongsTo(Role::class)->with('Permissions');
-        }
-
-        public function Images() {
-            return $this->hasMany(Image::class);
-        }
-
-        public function Posts() {
-            return $this->hasMany(Post::class);
-        }
-
-        public function Contributions() {
-            return $this->belongsToMany(Post::class, 'post_contributors');
-        }
-
-        public function Comments() {
-            return $this->hasMany(PostComment::class);
-        }
-
-        public function PostUpvotes() {
-            return $this->hasMany(PostUpvote::class);
-        }
-
-        public function CommentUpvotes() {
-            return $this->hasMany(PostCommentUpvote::class);
-        }
-
-        public function ReportedPosts() {
-            return $this->hasMany(ReportedPost::class);
-        }
-
-        public function ReportedComments() {
-            return $this->hasMany(ReportedPostComment::class);
-        }
-
-        public function Collections() {
-            return $this->hasMany(Collection::class);
-        }
-
-        public function EarnedBadges() {
-            return $this->hasMany(UserBadge::class)->where('earned', true)->with('Badge');
-        }
-
-        public function InProgressBadges() {
-            return $this->hasMany(UserBadge::class)->where('earned', false)->with('Badge');
-        }
-
-        public function Messages() {
-            return $this->hasMany(PrivateMessage::class);
+        public function Character() {
+            return $this->hasOne(UserCharacter::class);
         }
     }
