b51ccd7faaaaaa9b5e0db87e4785b2f011f59aa8
Ken <ken@syntaxflow.com>
2022-01-06 08:08:56 -0800

    Created app layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,35 +4,24 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <title>{{ config('app.name', 'Laravel') }}@hasSection('page-title') - @yield('page-title') @endif</title>
+        @include('components.og-tags')
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         @livewireStyles
-
+        <link rel="stylesheet" href="https://unpkg.com/trix@1.3.1/dist/trix.css">
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-
-            <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
         </div>
         @livewireScripts
+        <script src="https://unpkg.com/trix@1.3.1/dist/trix.js"></script>
     </body>
 </html>
