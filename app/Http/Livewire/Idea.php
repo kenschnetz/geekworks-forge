ab5f89dde313e7cdf8d7f48dfba694b5206387c4
Ken <ken@syntaxflow.com>
2022-01-08 13:43:19 -0800

    Streamlined idea controller

--- app/Http/Livewire/Idea.php
+++ app/Http/Livewire/Idea.php
@@ -4,12 +4,18 @@
 
     use App\Models\Post as PostModel;
     use App\Models\PostDetail as PostDetailsModel;
+    use App\Models\UserImage as UserImageModel;
+    use Illuminate\Database\Eloquent\Collection;
     use Livewire\Component;
+    use Str;
 
     class Idea extends Component {
         public int $post_id;
         public PostModel $post;
         public PostDetailsModel $post_details;
+        public UserImageModel|null $post_image;
+        public Collection $tags, $attributes, $actions;
+        protected $listeners = ['RefreshCollection', 'RefreshImage'];
 
         public function Mount() {
             $post_exists = PostModel::where('id', $this->post_id)->exists();
@@ -19,6 +25,10 @@
             } else {
                 abort(404);
             }
+            $this->RefreshImage();
+            $this->tags = $this->post_details->Tags()->with('Tag')->get();
+            $this->attributes = $this->post_details->Attributes()->with('Attribute')->get();
+            $this->actions = $this->post_details->Actions()->with('Action')->get();
         }
 
         public function Save($published) {
@@ -26,6 +36,8 @@
             $this->post->published = $published;
             $this->post->save();
             $this->post_details->save();
+            $this->SaveAttributeValues();
+            $this->SaveActionValues();
             redirect()->route('post', ['slug' => $this->post->slug]);
         }
 
@@ -33,6 +45,29 @@
             return view('livewire.idea');
         }
 
+        private function SaveAttributeValues() {
+            foreach(collect($this->attributes) as $attribute) {
+                $attribute->save();
+            }
+        }
+
+        private function SaveActionValues() {
+            foreach($this->actions as $action) {
+                $action->save();
+            }
+        }
+
+        public function RefreshCollection($collection) {
+            $Method = Str::ucfirst($collection);
+            $relationship = Str::singular($collection);
+            $this->$collection = $this->post_details->$Method()->with($relationship)->get();
+        }
+
+        public function RefreshImage() {
+            $this->post_image = $this->post_details->Images()->first();
+        }
+
+        // TODO: add custom error messages
         protected function Rules() {
             return [
                 'post.post_type_id' => 'required|integer',
@@ -43,11 +78,13 @@
                 'post.moderated' => 'required|boolean',
                 'post.allow_conversions' => 'required|boolean',
                 'post.is_art_only' => 'required|boolean',
-                'post_details.title' => 'required|string',
-                'post_details.description' => 'nullable|string',
+                'post_details.title' => 'required|string|max:255',
+                'post_details.description' => 'nullable|string|max:255',
                 'post_details.content' => 'nullable|string',
                 'post_details.requesting_recommendations' => 'required|boolean',
                 'post_details.requesting_conversions' => 'required|boolean',
+                'attributes.*.value' => 'nullable|string|max:255',
+                'actions.*.value' => 'nullable|string|max:255',
             ];
         }
     }
