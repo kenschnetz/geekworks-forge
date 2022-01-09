b5557f900e261b775e742bbd1bfe3245c4c24d41
Ken <ken@syntaxflow.com>
2022-01-08 23:10:17 -0800

    Created base modal blade

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,7 @@
+<div x-data="{show: @entangle($attributes->wire('model')).defer}" x-show="show" x-on:keydown.escape.window="show = false" x-cloak>
+    <div class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full" x-show="show">
+        <div class="w-full lg:w-2/3 xl:w-1/2 m-0 p-0 md:m-4 md:p-4 lg:m-auto bg-white shadow-2xl" style="max-height: 80% !important; overflow-x: hidden !important; overflow-y: scroll !important;">
+            {{ $slot }}
+        </div>
+    </div>
+</div>
