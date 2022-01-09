7e66abe98f37a6383d67396866d984385c9ebcf0
Ken <ken@syntaxflow.com>
2022-01-08 23:11:39 -0800

    Streamlined right sidebar

--- resources/views/components/right-sidebar.blade.php
+++ resources/views/components/right-sidebar.blade.php
@@ -1,22 +1,21 @@
-<aside>
-    <div class="sticky top-4">
-        <div class="bg-white rounded-lg shadow">
-            <div class="p-6">
-                <h2 class="text-center font-medium text-gray-500 uppercase tracking-wider inline-block align-middle">
-                    Top Posts
-                </h2>
-                <hr class="mt-3" />
-                <x-top-posts></x-top-posts>
-            </div>
-        </div>
-        <div class="mt-3 bg-white rounded-lg shadow">
-            <div class="p-6">
-                <h2 class="text-center font-medium text-gray-500 uppercase tracking-wider inline-block align-middle">
-                    Top Posters
-                </h2>
-                <hr class="mt-3" />
-                <x-top-posters></x-top-posters>
-            </div>
+<div class="sticky top-4">
+    <div class="bg-white shadow">
+        <div class="p-4">
+            <h2 class="text-center font-medium text-gray-500 uppercase tracking-wider inline-block align-middle">
+                Top Posts
+            </h2>
+            <hr class="mt-3" />
+            <x-top-posts></x-top-posts>
         </div>
     </div>
-</aside>
+    <div class="mt-3 bg-white shadow">
+        <div class="p-4">
+            <h2 class="text-center font-medium text-gray-500 uppercase tracking-wider inline-block align-middle">
+                Top Posters
+            </h2>
+            <hr class="mt-3" />
+            <x-top-posters></x-top-posters>
+        </div>
+    </div>
+</div>
+
