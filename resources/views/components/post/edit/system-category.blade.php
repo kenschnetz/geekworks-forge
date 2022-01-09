0e23af2cf87fde531430e0fe159db9225b04018b
Ken <ken@syntaxflow.com>
2022-01-08 23:09:31 -0800

    Removed unused blade files

--- resources/views/components/post/edit/system-category.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="flex space-x-6">
-    <div class="mt-3 bg-gray-200 rounded w-1/4 inline-block" style="height: 20px;"></div>
-    <span class="mx-1 py-2">-</span>
-    <div class="mt-3 bg-gray-200 rounded w-1/4 inline-block" style="height: 20px;"></div>
-</div>
