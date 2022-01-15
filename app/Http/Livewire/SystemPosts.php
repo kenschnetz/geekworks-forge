aa0165a1429da134ea01226bd4ab34a645b653dd
Ken <ken@syntaxflow.com>
2022-01-14 17:04:22 -0800

    Created system post list controller

--- app/Http/Livewire/SystemPosts.php
+++ app/Http/Livewire/SystemPosts.php
@@ -1,13 +1,20 @@
 <?php
 
-namespace App\Http\Livewire;
+    namespace App\Http\Livewire;
 
-use Livewire\Component;
+    use App\Models\System as SystemModel;
 
-class SystemPosts extends Component
-{
-    public function render()
-    {
-        return view('livewire.system-posts');
+    class SystemPosts extends Feed {
+        public string $system_slug;
+
+        public function Mount() {
+            $system = SystemModel::where('slug', $this->system_slug)->first();
+            if (empty($system)) {
+                abort(404);
+            }
+            $this->system_id = $system->id;
+            $this->pagination_count = config('app.settings.post_pagination', 20);
+            array_push($this->filters, 'System: ' . $system->name);
+            $this->menu_show_systems = true;
+        }
     }
-}
