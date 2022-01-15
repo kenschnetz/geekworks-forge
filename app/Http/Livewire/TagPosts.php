8cd47e24a4538bbeb0b474e3d5090858a75fc592
Ken <ken@syntaxflow.com>
2022-01-14 17:04:51 -0800

    Created tag post list controller

--- app/Http/Livewire/TagPosts.php
+++ app/Http/Livewire/TagPosts.php
@@ -1,13 +1,19 @@
 <?php
 
-namespace App\Http\Livewire;
+    namespace App\Http\Livewire;
 
-use Livewire\Component;
+    use App\Models\Tag as TagModel;
 
-class TagPosts extends Component
-{
-    public function render()
-    {
-        return view('livewire.tag-posts');
+    class TagPosts extends Feed {
+        public string $tag_slug;
+
+        public function Mount() {
+            $tag = TagModel::where('slug', $this->tag_slug)->first();
+            if (empty($tag)) {
+                abort(404);
+            }
+            $this->tag_id = $tag->id;
+            $this->pagination_count = config('app.settings.post_pagination', 20);
+            array_push($this->filters, 'Tag: ' . $tag->name);
+        }
     }
-}
