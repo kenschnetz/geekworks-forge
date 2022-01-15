5a80c5c3c40547f231f7d975e644fb5d4f260edd
Ken <ken@syntaxflow.com>
2022-01-14 17:03:02 -0800

    Completed idea edit controller

--- app/Http/Livewire/Idea.php
+++ app/Http/Livewire/Idea.php
@@ -3,19 +3,10 @@
     namespace App\Http\Livewire;
 
     use App\Models\Post as PostModel;
-    use App\Models\PostDetail as PostDetailsModel;
-    use App\Models\UserImage as UserImageModel;
-    use Illuminate\Database\Eloquent\Collection;
-    use Livewire\Component;
-    use Str;
+    use App\Utilities\Post as PostUtilities;
+    use Illuminate\Validation\Rule;
 
-    class Idea extends Component {
-        public int $post_id;
-        public PostModel $post;
-        public PostDetailsModel $post_details;
-        public UserImageModel|null $post_image;
-        public Collection $tags, $attributes, $actions;
-        protected $listeners = ['RefreshCollection', 'RefreshImage'];
+    class Idea extends EditPost {
 
         public function Mount() {
             $post_exists = PostModel::where('id', $this->post_id)->exists();
@@ -25,46 +16,20 @@
             } else {
                 abort(404);
             }
+            $this->images = PostUtilities::GetMeta($this->post_details, 'image');
+            $this->removed_images = [];
+            $this->tags = PostUtilities::GetMeta($this->post_details, 'tag');
+            $this->removed_tags = [];
+            $this->attributes = PostUtilities::GetMeta($this->post_details, 'attribute');
+            $this->removed_attributes = PostUtilities::GetMeta($this->post_details, 'attribute', true);
+            $this->actions = PostUtilities::GetMeta($this->post_details, 'action');
+            $this->removed_actions = PostUtilities::GetMeta($this->post_details, 'action', true);
             $this->RefreshImage();
-            $this->tags = $this->post_details->Tags()->with('Tag')->get();
-            $this->attributes = $this->post_details->Attributes()->with('Attribute')->get();
-            $this->actions = $this->post_details->Actions()->with('Action')->get();
-        }
-
-        public function Save($published) {
-            $this->validate();
-            $this->post->published = $published;
-            $this->post->save();
-            $this->post_details->save();
-            $this->SaveAttributeValues();
-            $this->SaveActionValues();
-            redirect()->route('post', ['slug' => $this->post->slug]);
+            $this->post_type = 'idea';
         }
 
         public function Render() {
-            return view('livewire.idea');
-        }
-
-        private function SaveAttributeValues() {
-            foreach(collect($this->attributes) as $attribute) {
-                $attribute->save();
-            }
-        }
-
-        private function SaveActionValues() {
-            foreach($this->actions as $action) {
-                $action->save();
-            }
-        }
-
-        public function RefreshCollection($collection) {
-            $Method = Str::ucfirst($collection);
-            $relationship = Str::singular($collection);
-            $this->$collection = $this->post_details->$Method()->with($relationship)->get();
-        }
-
-        public function RefreshImage() {
-            $this->post_image = $this->post_details->Images()->first();
+            return view('livewire.edit-post');
         }
 
         // TODO: add custom error messages
@@ -78,13 +43,19 @@
                 'post.moderated' => 'required|boolean',
                 'post.allow_conversions' => 'required|boolean',
                 'post.is_art_only' => 'required|boolean',
-                'post_details.title' => 'required|string|max:255',
+                'post_details.title' => [
+                    'required',
+                    'string',
+                    'max:255',
+                    Rule::unique('post_details', 'title')->ignore(optional($this->post_details)->id)
+                ],
                 'post_details.description' => 'nullable|string|max:255',
                 'post_details.content' => 'nullable|string',
                 'post_details.requesting_recommendations' => 'required|boolean',
                 'post_details.requesting_conversions' => 'required|boolean',
                 'attributes.*.value' => 'nullable|string|max:255',
                 'actions.*.value' => 'nullable|string|max:255',
+                'tags' => 'required|array|min:3',
             ];
         }
     }
