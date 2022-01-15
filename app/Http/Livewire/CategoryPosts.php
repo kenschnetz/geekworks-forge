1018d39cb430690f8b4fabe4c427d1aaac628a28
Ken <ken@syntaxflow.com>
2022-01-14 17:01:47 -0800

    Created category post list controller

--- app/Http/Livewire/CategoryPosts.php
+++ app/Http/Livewire/CategoryPosts.php
@@ -1,13 +1,20 @@
 <?php
 
-namespace App\Http\Livewire;
+    namespace App\Http\Livewire;
 
-use Livewire\Component;
+    use App\Models\Category as CategoryModel;
 
-class CategoryPosts extends Component
-{
-    public function render()
-    {
-        return view('livewire.category-posts');
+    class CategoryPosts extends Feed {
+        public string $category_slug;
+
+        public function Mount() {
+            $category = CategoryModel::where('slug', $this->category_slug)->first();
+            if (empty($category)) {
+                abort(404);
+            }
+            $this->category_id = $category->id;
+            $this->pagination_count = config('app.settings.post_pagination', 20);
+            array_push($this->filters, 'Category: ' . $category->name);
+            $this->menu_show_categories = true;
+        }
     }
-}
