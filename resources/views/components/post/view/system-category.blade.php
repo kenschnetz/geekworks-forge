ebac8ca1c4b2ef1e513df8fe5f2a8619cc47728c
Ken <ken@syntaxflow.com>
2022-01-14 17:10:16 -0800

    Streamlined post view system category

--- resources/views/components/post/view/system-category.blade.php
+++ resources/views/components/post/view/system-category.blade.php
@@ -1,11 +1,17 @@
 <div class="flex space-x-6">
     <p class="text-sm text-gray-400">
-        <a href="{{ route('system-posts', ['system_slug' => $post->System->slug]) }}" class="hover:underline">
-            {{ $post->System->name }}
-        </a>
-        <span class="mx-1">-</span>
-        <a href="{{ route('category-posts', ['category_slug' => $post->Category->slug]) }}" class="hover:underline">
-            {{ $post->Category->name }}
-        </a>
+        @if($post->System->id !== 1)
+            <a href="{{ route('system-posts', ['system_slug' => $post->System->slug]) }}" class="hover:underline">
+                {{ $post->System->name }}
+            </a>
+        @endif
+        @if($post->System->id !== 1 && $post->Category->id !== 1)
+            <span class="mx-1">-</span>
+        @endif
+        @if($post->Category->id !== 1)
+            <a href="{{ route('category-posts', ['category_slug' => $post->Category->slug]) }}" class="hover:underline">
+                {{ $post->Category->name }}
+            </a>
+        @endif
     </p>
 </div>
