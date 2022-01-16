64f5585a1bd11456b968db2e1f4fc22421955edc
Ken <ken@syntaxflow.com>
2022-01-15 23:15:49 -0800

    Streamlined post stats blade

--- resources/views/components/post/view/stats.blade.php
+++ resources/views/components/post/view/stats.blade.php
@@ -1,14 +1,21 @@
-<div class="flex space-x-6">
-    <div class="inline-flex flex items-center space-x-2 text-sm text-gray-300">
-        <i class="fas fa-thumbs-up"></i>
-        <span class="font-semi-bold text-gray-500">{{ $post->upvotes_count }}</span>
+<div class="w-full">
+    <div class="flex">
+        <div class="inline-block flex items-center space-x-2  text-sm text-gray-300 mr-3">
+            <i class="fas fa-thumbs-up"></i>
+            <span class="font-semi-bold text-gray-500">{{ App\Utilities\Number::Shorten($post->upvotes_count) }}</span>
+        </div>
+        <div class="inline-block flex items-center space-x-2  text-sm text-gray-300 mr-3">
+            <i class="fas fa-comment-alt"></i>
+            <span class="font-semi-bold text-gray-500">{{ App\Utilities\Number::Shorten($post->comments_count) }}</span>
+        </div>
+        <div class="inline-block flex items-center space-x-2  text-sm text-gray-300 mr-3">
+            <i class="fas fa-eye"></i>
+            <span class="font-semi-bold text-gray-500">{{ App\Utilities\Number::Shorten($post->views_count) }}</span>
+        </div>
+        {{--TODO: figure out where to put this--}}
     </div>
-    <div class="inline-flex flex items-center space-x-2 text-sm text-gray-300">
-        <i class="fas fa-comment-alt"></i>
-        <span class="font-semi-bold text-gray-500">{{ $post->comments_count }}</span>
-    </div>
-    <div class="inline-flex flex items-center space-x-2 text-sm text-gray-300">
-        <i class="fas fa-eye"></i>
-        <span class="font-semi-bold text-gray-500">{{ $post->views_count }}</span>
-    </div>
+{{--    <div class="text-sm text-gray-300 text-right">--}}
+{{--        {{ $post->created_at->diffForHumans() }}--}}
+{{--    </div>--}}
 </div>
+
