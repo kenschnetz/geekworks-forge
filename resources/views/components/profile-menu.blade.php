3b059839d3363672fc17c5b13d8dd4da69ac4015
Ken <ken@syntaxflow.com>
2022-01-15 23:16:01 -0800

    Completed profile menu blade

--- resources/views/components/profile-menu.blade.php
+++ resources/views/components/profile-menu.blade.php
@@ -5,21 +5,27 @@
             Your Profile
         </span>
     </a>
+    <a href="{{route('account')}}" class="text-gray-900 group flex items-center px-3 py-2">
+        <i class="fal fa-cog text-gray-900 text-center" style="font-size: 1.4em !important; width: 32px !important;"></i>
+        <span class="ml-3 font-medium text-gray-500 tracking-wider inline-block align-middle hover:underline">
+            Your Account
+        </span>
+    </a>
     <a href="{{route('messenger')}}" class="text-gray-900 group flex items-center px-3 py-2">
         <i class="fal fa-comments text-gray-900 text-center" style="font-size: 1.4em !important; width: 32px !important;"></i>
         <span class="ml-3 font-medium text-gray-500 tracking-wider inline-block align-middle hover:underline">
             Messenger
         </span>
         @if(auth()->user()->MessengerNotifications())
-        <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full border border-white ml-2">
-            {{ auth()->user()->MessengerNotifications() < 1000 ? auth()->user()->MessengerNotifications() : 999 . '+' }}
-        </span>
+            <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full border border-white ml-2">
+                {{ auth()->user()->MessengerNotifications() < 1000 ? auth()->user()->MessengerNotifications() : 999 . '+' }}
+            </span>
         @endif
     </a>
 {{--TODO: add notifications here--}}
     @if(Auth()->user()->IsAdmin())
         <a href="{{route('admin-tools')}}" class="text-gray-900 group flex items-center px-3 py-2">
-            <i class="fal fa-cog text-gray-900 text-center" style="font-size: 1.4em !important; width: 32px !important;"></i>
+            <i class="fal fa-user-crown text-gray-900 text-center" style="font-size: 1.4em !important; width: 32px !important;"></i>
             <span class="ml-3 font-medium text-gray-500 tracking-wider inline-block align-middle hover:underline">
                 Admin Tools
             </span>
