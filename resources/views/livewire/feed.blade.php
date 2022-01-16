70f1707359dc66ce548a90e0e89ea60b4673b41f
Ken <ken@syntaxflow.com>
2022-01-15 23:19:10 -0800

    Streamlined feed view

--- resources/views/livewire/feed.blade.php
+++ resources/views/livewire/feed.blade.php
@@ -1,13 +1,13 @@
 <div class="max-w-2xl mx-auto sm:px-4 lg:max-w-7xl lg:grid lg:grid-cols-12 lg:gap-4">
     <div class="hidden lg:block lg:col-span-3">
-        @include('components.left-sidebar')
+        @include('components.feed.left-sidebar')
     </div>
     <main class="lg:col-span-6">
         <div class="px-4 sm:p-0">
             @include('components.list-search')
         </div>
         <div class="mt-3 px-4 sm:p-0 lg:hidden">
-            @include('components.right-sidebar')
+            @include('components.feed.right-sidebar')
         </div>
         <div class="mt-3 px-4 sm:p-0">
             @if($posts->count() > 0)
@@ -18,6 +18,10 @@
                         </li>
                     @endforeach
                 </ul>
+                @if ($posts->hasPages())
+                    <hr class="my-3" />
+                    {{ $posts->links() }}
+                @endif
             @else
                 <div class="bg-white p-4 shadow sm:p-6">
                     No posts Found!
@@ -26,6 +30,6 @@
         </div>
     </main>
     <div class="hidden lg:block lg:col-span-3">
-        @include('components.right-sidebar')
+        @include('components.feed.right-sidebar')
     </div>
 </div>
