5b627f22a729717065eab9fb760e549b6eb6b665
Ken <ken@syntaxflow.com>
2022-01-15 23:12:20 -0800

    Refactored author blade to be more universal

--- resources/views/components/author.blade.php
+++ resources/views/components/author.blade.php
@@ -1,13 +1,25 @@
-<div class="flex-shrink-0">
-    <a href="{{ route('user-profile', ['user_id' => $author['id']]) }}">
-        <img class="h-10 w-10 rounded-full" src="{{ ($author['character']['profile_photo']['path']) ?? '/storage/img/default-profile.jpg' }}" alt="">
-    </a>
-</div>
-<div class="min-w-0 flex-1">
-    <p class="text-sm font-medium text-gray-900">
-        <a href="{{ route('user-profile', ['user_id' => ($author['id'])]) }}" class="hover:underline font-medium text-purple-700">{{ $author['character']['name'] }}</a>
-    </p>
-    <p class="text-sm text-gray-500">
-        Level {{ $author['character']['level'] }}
-    </p>
+<div class="flex space-x-3">
+    @if(!empty($author))
+        <div class="flex-shrink-0">
+            <a href="{{ route('user-profile', ['user_name' => Str::lower($author->Character->name)]) }}">
+                <img class="h-10 w-10 rounded-full" src="{{ $author->Character->ProfilePhoto->path ?? '/storage/img/default-profile.jpg' }}" alt="">
+            </a>
+        </div>
+        <div class="min-w-0 flex-1">
+            <p class="text-sm font-medium text-gray-900">
+                <a href="{{ route('user-profile', ['user_name' => Str::lower($author->Character->name)]) }}" class="hover:underline font-medium text-purple-700">{{ $author->Character->name }}</a>
+            </p>
+            <p class="text-sm text-gray-500">
+                Level {{ $author->Character->level }}
+            </p>
+        </div>
+    @else
+        <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" src="{{ '/storage/img/default-profile.jpg' }}" alt="">
+        </div>
+        <div class="min-w-0 flex-1">
+            <div class="mt-1 bg-gray-100 rounded w-2/3" style="height: 20px;"></div>
+            <div class="mt-1 bg-gray-100 rounded w-1/3" style="height: 20px;"></div>
+        </div>
+    @endif
 </div>
