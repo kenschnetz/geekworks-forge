d8eb430e91f30ee2d66078f2c00a8ed98f0181ac
Ken <ken@syntaxflow.com>
2022-01-15 23:18:30 -0800

    Created canonize modal

---
+++ resources/views/livewire/canonize-modal.blade.php
@@ -0,0 +1,8 @@
+<div class="absolute top-0 left-0" x-data="{ tooltip: 'Hover or click an item to see it\'s description', default_tooltip: 'Hover or click an item to see it\'s description' }">
+    <x-modal wire:model="show">
+        <div class="my-3 px-4 sm:px-0">
+            <p class="text-xl font-bold">Canonize Post</p>
+        </div>
+        <hr class="my-3" />
+    </x-modal>
+</div>
