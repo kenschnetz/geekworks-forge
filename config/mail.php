a9a0a04bac92b8ecd5391696d5dcf9fe1ef2716b
Ken <ken@syntaxflow.com>
2022-02-01 16:15:00 -0800

    Reset mail config

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_DRIVER', 'sendmail'),
+    'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
@@ -114,13 +114,5 @@ return [
             resource_path('views/vendor/mail'),
         ],
     ],
-
-    'stream' => [
-        'ssl' => [
-            'allow_self_signed' => true,
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-        ],
-    ]
 
 ];
