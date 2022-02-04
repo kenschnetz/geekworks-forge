3d4c8ebb941c6670c8852bdc9b9fd9d71cd8a47a
Ken <ken@syntaxflow.com>
2022-02-04 15:31:09 -0800

    Removed space from the Canon sidebar components

--- resources/views/components/canon/view/left-sidebar.blade.php
+++ resources/views/components/canon/view/left-sidebar.blade.php
@@ -1,13 +1,14 @@
 <x-sidebar>
     <div class="bg-white dark:bg-zinc-700 shadow p-4">
         <x-author :author="$item->User"></x-author>
+        @if($item->user_id === auth()->user()->id && !$editing)
+            <hr class="mt-3" />
+            <div class="mt-3">
+                <span class="text-gray-900 group flex items-center" wire:click="ToggleEditing()">
+                    <i class="mr-3 fas fa-edit text-purple-800"></i>
+                    <span class="hover:underline">Edit</span>
+                </span>
+            </div>
+        @endif
     </div>
-    @if($item->user_id === auth()->user()->id && !$editing)
-        <div class="mt-4 bg-white dark:bg-zinc-700 shadow p-4">
-            <span class="text-gray-900 group flex items-center" wire:click="ToggleEditing()">
-                <i class="mr-3 fas fa-edit text-purple-800"></i>
-                <span class="hover:underline">Edit</span>
-            </span>
-        </div>
-    @endif
 </x-sidebar>
