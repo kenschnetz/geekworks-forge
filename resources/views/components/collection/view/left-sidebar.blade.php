e99281c462d9848c3b1ea39110ff8736dc9fe8de
Ken <ken@syntaxflow.com>
2022-02-04 15:31:57 -0800

    Removed space from the Collection sidebar components

--- resources/views/components/collection/view/left-sidebar.blade.php
+++ resources/views/components/collection/view/left-sidebar.blade.php
@@ -1,13 +1,11 @@
 <x-sidebar>
     <div class="bg-white dark:bg-zinc-700 shadow p-4">
         <x-author :author="$item->User"></x-author>
-    </div>
-    @if($item->user_id === auth()->user()->id && !$editing)
-        <div class="mt-4 bg-white dark:bg-zinc-700 shadow p-4">
+        @if($item->user_id === auth()->user()->id && !$editing)
             <span class="text-gray-900 group flex items-center" wire:click="ToggleEditing()">
                 <i class="mr-3 fas fa-edit text-purple-800"></i>
                 <span class="hover:underline">Edit</span>
             </span>
-        </div>
-    @endif
+        @endif
+    </div>
 </x-sidebar>
