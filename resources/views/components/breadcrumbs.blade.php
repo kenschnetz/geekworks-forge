d10b4f3a347edceea1f9be03dfcd93e972f310b8
Ken <ken@syntaxflow.com>
2022-02-04 15:27:39 -0800

    Created breadcrumb insertion

---
+++ resources/views/components/breadcrumbs.blade.php
@@ -0,0 +1,31 @@
+<div class="max-w-3xl mx-auto px-4 lg:max-w-7xl">
+    <div class="bg-white dark:bg-zinc-700 mt-3 flex p-4 shadow" aria-label="Breadcrumb">
+        <ol role="list" class="flex items-center space-x-4">
+            <li>
+                <div>
+                    <a href="{{ route('home') }}" class="text-purple-600 hover:text-gray-500 p-4">
+                        <i class="fas fa-home-alt"></i>
+                    </a>
+                </div>
+            </li>
+            @foreach($breadcrumbs as $index => $breadcrumb)
+                <li>
+                    <i class="fal fa-chevron-left fa-flip-horizontal text-gray-400"></i>
+                </li>
+                <li>
+                    <div>
+                        @if(!$loop->last)
+                            <a href="{{ route($breadcrumb['route'], $breadcrumb['route_params'] ?? []) }}" class="text-purple-600 hover:text-gray-500 p-4 text-sm">
+                                {{ $breadcrumb['name'] }}
+                            </a>
+                        @else
+                            <span class="p-4 text-sm">
+                                {{ $breadcrumb['name'] }}
+                            </span>
+                        @endif
+                    </div>
+                </li>
+            @endforeach
+        </ol>
+    </div>
+</div>
