b43d53cdbbb5579a5949656704f9be42ce7a95bb
Ken <ken@syntaxflow.com>
2022-02-04 15:32:17 -0800

    Cleaned up search bar

--- resources/views/components/list-search.blade.php
+++ resources/views/components/list-search.blade.php
@@ -1,11 +1,10 @@
 <div class="flex items-center">
     <div class="w-full">
-        <label for="search" class="sr-only">Search</label>
         <div class="relative">
             <div class="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
-                <i class="fas fa-search dark:text-gray-400"></i>
+                <i class="fas fa-search text-gray-300 dark:text-gray-400"></i>
             </div>
-            <input wire:model="search_term" id="search" name="search" class="block w-full bg-white dark:bg-zinc-900 dark:text-gray-400 border border-gray-200 py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-gray-400 sm:text-sm" placeholder="Search" type="search">
+            <input wire:model="search_term" id="search" name="search" class="block w-full bg-white dark:bg-zinc-900 dark:text-gray-400 border border-gray-200 py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-gray-400 sm:text-sm" type="search">
         </div>
     </div>
 </div>
