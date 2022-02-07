0db64c969da20c6471dcb3455e6d83cfe390973e
Ken <ken@syntaxflow.com>
2022-02-06 22:46:31 -0800

    Added option for hiding buttons if the admin edit screen is empty

--- resources/views/components/admin-tools/edit.blade.php
+++ resources/views/components/admin-tools/edit.blade.php
@@ -4,13 +4,15 @@
     </h2>
     <hr class="mt-3" />
     {{ $slot }}
-    <hr class="mt-3" />
-    <div class="w-full mt-3 text-right">
-        <button wire:click="Cancel()" class="bg-gray-400 hover:bg-transparent text-white hover:text-gray-400 font-bold px-4 py-3 border border-gray-600 w-full sm:w-1/4" style="width:140px">
-            Cancel
-        </button>
-        <button wire:click="Save()" class="bg-purple-800 hover:bg-transparent text-white hover:text-purple-800 font-bold px-4 py-3 border border-purple-800 w-full sm:w-1/4" style="width:140px">
-            Save
-        </button>
-    </div>
+    @if(!$hideButtons ?? true)
+        <hr class="mt-3" />
+        <div class="w-full mt-3 text-right">
+            <button wire:click="Cancel()" class="bg-gray-400 hover:bg-transparent text-white hover:text-gray-400 font-bold px-4 py-3 border border-gray-600 w-full sm:w-1/4" style="width:140px">
+                Cancel
+            </button>
+            <button wire:click="Save()" class="bg-purple-800 hover:bg-transparent text-white hover:text-purple-800 font-bold px-4 py-3 border border-purple-800 w-full sm:w-1/4" style="width:140px">
+                Save
+            </button>
+        </div>
+    @endif
 </div>
