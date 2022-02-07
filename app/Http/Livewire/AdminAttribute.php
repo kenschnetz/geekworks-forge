80045046dd443b6695dc513028195750a1dd17c9
Ken <ken@syntaxflow.com>
2022-02-06 22:44:55 -0800

    Added admin attribute edit screen

---
+++ app/Http/Livewire/AdminAttribute.php
@@ -0,0 +1,11 @@
+<?php
+
+    namespace App\Http\Livewire;
+
+    use Livewire\Component;
+
+    class AdminAttribute extends Component {
+        public function Render() {
+            return view('livewire.admin-attribute');
+        }
+    }
---
+++ resources/views/livewire/admin-attribute.blade.php
@@ -0,0 +1,5 @@
+<x-admin-tools.edit :title="'Attribute'" :list_route="'admin-attributes'" :hide_buttons="true">
+    <div class="w-full mt-3">
+        Feature coming soon!
+    </div>
+</x-admin-tools.edit>
